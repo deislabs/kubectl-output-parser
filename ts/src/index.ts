09c0d9ee2d903513f96f5e4c7dff91bc76fe1c5a
itowlson <github@hestia.cc>
2019-05-06 16:40:24 +1200

    Reorganising internals

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,6 +1,3 @@
-import { Errorable, failed } from "./errorable";
-import { Dictionary } from "./dictionary";
-
 export interface ShellResult {
     readonly code: number;
     readonly stdout: string;
@@ -9,54 +6,6 @@ export interface ShellResult {
 
 export type KubectlOutput = ShellResult | undefined;
 
-const KUBECTL_OUTPUT_COLUMN_SEPARATOR = /\s+/g;
-
-export interface TableLines {
-    readonly header: string;
-    readonly body: string[];
-}
-
-export function asHeaderedLines(output: KubectlOutput): Errorable<TableLines> {
-    if (!output) {
-        return { succeeded: false, reason: 'failed-to-run', error: 'Unable to run kubectl' };
-    }
-
-    if (output.code === 0) {
-        const lines = output.stdout.split('\n');
-        if (lines.length === 0) {
-            return { succeeded: true, result: { header: '', body: [] } };
-        }
-        const header = lines.shift()!;  // safe because we have dealt with the length === 0 case
-        const body = lines.filter((l) => l.length > 0);
-        return { succeeded: true, result: { header, body } };
-    }
-
-    return { succeeded: false, reason: 'kubectl-error', error: output.stderr };
-}
-
-export function parseHeaderedLines(output: KubectlOutput): Errorable<Dictionary<string>[]> {
-    const table = asHeaderedLines(output);
-    if (failed(table)) {
-        return table;
-    }
-
-    const parsed = parseTable(table.result, KUBECTL_OUTPUT_COLUMN_SEPARATOR);
-    return { succeeded: true, result: parsed };
-}
-
-export function parseTable(table: TableLines, columnSeparator: RegExp): Dictionary<string>[] {
-    if (table.header.length === 0 || table.body.length === 0) {
-        return [];
-    }
-    const columnHeaders = table.header.toLowerCase().replace(columnSeparator, '|').split('|');
-    return table.body.map((line) => parseLine(line, columnHeaders, columnSeparator));
-}
-
-function parseLine(line: string, columnHeaders: string[], columnSeparator: RegExp) {
-    const lineInfoObject = Dictionary.of<string>();
-    const bits = line.replace(columnSeparator, '|').split('|');
-    bits.forEach((columnValue, index) => {
-        lineInfoObject[columnHeaders[index].trim()] = columnValue.trim();
-    });
-    return lineInfoObject;
-}
+export * from './dictionary';
+export * from './errorable';
+export { asHeaderedLines, parseHeaderedLines, parseTable, TableLines } from './table';
